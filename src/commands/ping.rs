72bf6e91a8685400a778396a15bf039164306757
Akiyatamashii <z33165338x@gmail.com>
2024-09-25 21:14:46 +0800

    update struck

--- src/commands/ping.rs
+++
@@ -1,9 +0,0 @@
-use serenity::{builder::CreateCommand, model::application::ResolvedOption};
-
-pub fn register() -> CreateCommand {
-    CreateCommand::new("ping").description("連線測試")
-}
-
-pub fn run(_options: &[ResolvedOption]) -> String {
-    "Pong!".to_string()
-}
